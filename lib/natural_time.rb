159bef92a0c1c768dd69f34b161f2d27bbf107cf
rynkwn <rynkwn@gmail.com>
2016-06-12 17:58:03 +0000

    Began implementing methods.

--- lib/natural_time.rb
+++ lib/natural_time.rb
@@ -2,4 +2,17 @@ class NaturalTime
   def self.hi
     puts "Hello world!"
   end
+  
+  
+  # Public: Parse the text and return interpreted dates and times.
+  #
+  # txt  - The String to be duplicated.
+  # default_date - The Integer number of times to duplicate the text.
+  # unique - Boolean flag. Should we filter out duplicate dates?
+  #
+  # Returns an array of Date
+  # From a Daily Message, grab date in the natural message, if possible.
+  # Otherwise, default to my provided date.
+  def parse(txt, default_date=nil, unique=false)
+  end
 end
