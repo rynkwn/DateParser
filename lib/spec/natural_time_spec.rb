4ef0d5acee5846fb72c9c8c547519a2d8f7f2617
rynkwn <rynkwn@gmail.com>
2016-06-21 23:17:43 +0000

    Started writing a few tests for NaturalDateParsing using Rspec.

--- lib/spec/natural_time_spec.rb
+++ lib/spec/natural_time_spec.rb
@@ -1,9 +1,30 @@
 require_relative "../natural_time"
 
+## Run this file with this command:
+## rspec lib/spec/natural_time_spec.rb
 
-
-#describe MyModel do
-  #it "should be true" do
-    #MyModel.new.the_truth.should be_true
-  #end
-#end
+describe NaturalDateParsing do
+  ## I probably want everything to always be wrapped in an array/list.
+  ## Consistency is valuable to an end user.
+  
+  before do
+    @date = "April 6th, 2014"
+    @text = "Remember to meet me on April 6th, 2014, alright?"
+    
+    @parsed_date = [Date.parse("April 6th, 2014")]
+  end
+  
+  describe ".interpret_date" do
+    context "given 'April 6th, 2014'" do
+      it "returns Sun, 06 Apr 2014 as a date object" do
+        expect(NaturalDateParsing::interpret_date(@date)).to eql(@parsed_date)
+      end
+    end
+    
+    context "given a sentence containing April 6th, 2014" do
+      it "returns Sun, 06 Apr 2014 as a date object" do 
+        expect(NaturalDateParsing::interpret_date(@text)).to eql(@parsed_date)
+      end
+    end
+  end
+end
