7795480cdcca8d954e88ec43d487968f21e042f4
rynkwn <rynkwn@gmail.com>
2016-07-24 02:45:59 +0000

    In the middle of documenting.

--- lib/date_parser.rb
+++ lib/date_parser.rb
@@ -9,53 +9,33 @@ require_relative 'date_parser/utils'
 # *parse(txt, options)*: Parse a block of text and return an array of the parsed
 # dates as Date objects.
 #
-# *get_first_date(txt): Parse a block of text but stop and return the first parsed
-# date found.
+# == Examples:
+#
+# 
 #
 
 module DateParser
-  
-  # Public: Parse the text and return interpreted dates and times.
-  #
-  # txt  - The String to be duplicated.
-  # unique - Boolean flag. Should we filter out duplicate dates?
-  #
-  # Returns an array of Date
-  # From a Daily Message, grab date in the natural message, if possible.
-  # Otherwise, default to my provided date.
-  
-  ## Maybe also want a default date?
-  
-  # 
   
   # Parses a text object and returns an array of parsed dates.
   #
   # ==== Attributes
   #
-  # * +txt+ - 
-  # * +txt+ - 
-  # * +txt+ - 
+  # * +txt+ - The text to parse.
+  # * +creation_date+ - The date the text was created or released. Defaults to nil,
+  # but if provided can make returned dates more accurate.
   #
   # ==== Options
   #
-  # You may which to break out options as a separate item since there maybe
-  # multiple items. Note options are prefixed with a colon, denoting them
-  # as a 
-  #
-  # * +:conditions+ - An SQL fragment like "administrator = 1"
-  # * +:order+ - An SQL fragment like "created_at DESC, name".
-  # * +:group+ - An attribute name by which the result should be grouped
-  # * +:limit+ - An integer determining the limit on the number of rows that should be returned.
-  # * +:offset+ - An integer determining the offset from where the rows should be fetched.
-  # * +:joins+ - Either an SQL fragment for additional joins like "LEFT JOIN comments ON comments.post_id = id" (rarely needed)
+  # * +:unique+ - Return only unique Date objects. Defaults to false
+  # * +:nil_date+ - A date to return if no dates are parsed. Defaults to nil.
   #
   # ==== Examples
-  # 
-  # Illustrate the behaviour of the method using examples. Indent examples:
   #
-  #    base = Base.new("Example String")
+  #    
   #    base.method_name("Example", "more")
-  def DateParser.parse(txt, unique = false, default_date = nil)
+  def DateParser.parse(txt, creation_date = nil, opts = {})
+    unique = opts[:unique] || false
+    nil_date = opts[:default_date] || nil
     
     interpreted_dates = NaturalDateParsing::interpret_date(txt, Date.today)
     
@@ -68,9 +48,5 @@ module DateParser
     end
     
     return interpreted_dates
-  end
-  
-  def DateParser.get_first_date(txt)
-    return NaturalDateParsing::interpret_first_date(txt)
   end
 end
--- lib/spec/date_parser_spec.rb
+++ lib/spec/date_parser_spec.rb
@@ -61,72 +61,72 @@ describe NaturalDateParsing do
         "Domingo, Dominican Republic where Francisco del Rosario Sánchez, one of the" + 
         "Dominican Founding Fathers, proclaimed Dominican independence and raised the" + 
         "first Dominican Flag, on February 27, 1844."
-      released = nil
+      creation_date = nil
       answer = [Date.parse("February 27, 1844")]
       
       it "captures the single date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
     context "Colloquial use of date 1" do
       text = "We should go on the 4th if we can."
-      released = Date.parse("July 1st 2016")
+      creation_date = Date.parse("July 1st 2016")
       answer = [Date.parse("July 4th, 2016")]
       
-      it "correctly uses the released parameter" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+      it "correctly uses the creation_date parameter" do
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
     context "Correct use of XX/XX format 1" do
       text = "Newsflash: things happen on 02/12!"
-      released = Date.parse("January 1st, 1994")
+      creation_date = Date.parse("January 1st, 1994")
       answer = [Date.parse("February 12, 1994")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
     context "Given yesterday" do
       text = "Yesterday was certainly a day."
-      released = nil
+      creation_date = nil
       answer = [Date.today - 1]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
-    context "Given yesterday with a released date" do
+    context "Given yesterday with a creation_date date" do
       text = "Yesterday was certainly a day."
-      released = Date.parse("January 12, 1994")
+      creation_date = Date.parse("January 12, 1994")
       answer = [Date.parse("January 11, 1994")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
     context "Single year is correctly parsed" do
       text = "In 1492 Columbus sailed the ocean blue."
-      released = nil
+      creation_date = nil
       answer = [Date.parse("January 1, 1492")]
       parse_single_years = true
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released, parse_single_years)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date, parse_single_years)).to eql(answer)
       end
     end
     
     context "Correctly parses month and day in middle of sentence" do
       text = "Something something something march 4 something something"
-      released = Date.parse("Jan 1, 2004")
+      creation_date = Date.parse("Jan 1, 2004")
       answer = [Date.parse("March 4, 2004")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
@@ -146,12 +146,12 @@ describe NaturalDateParsing do
     context "Colloquial example 1" do
       text = "Charlie Chaplin and Jason Earles (Hannah Montana's brother) were " +
       "alive at the same time for eight months in 1977"
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("January 1, 1977")]
       parse_single_years = true
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released, parse_single_years)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date, parse_single_years)).to eql(answer)
       end
     end
     
@@ -161,21 +161,21 @@ describe NaturalDateParsing do
     #  text = "For a job I started this April, I had to parse dates of various " +
     #  "formats, such as MM-DD-YY, MM/YYYY, and YY-MM-DD. It was infuriating, " +
     #  "and what I assume you to be doing reminds me of this."
-    #  released = Date.parse("July 6, 2016")
+    #  creation_date = Date.parse("July 6, 2016")
     #  answer = [Date.parse("April 1, 2016")] ## Reconsider
     #  
     #  it "correctly grabs the date" do
-    #    expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+    #    expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
     #  end
     #end
     
     context "Colloquial example 3" do
       text = "August 25, 2013, I met So-and-So"
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("August 25, 2013")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
@@ -183,32 +183,32 @@ describe NaturalDateParsing do
       text = "Quincy Jones (producer of Thriller) and Michael Caine (veteran " +
       "British actor) were both born on the same day and the same hour on March " +
       "14, 1933. They are still friends to this day."
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("March 14, 1933")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
     context "Colloquial example 5" do
       text = "Two days ago (July 4, 2016) was the 190th death anniversary of " +
       "the second and third US Presidents: John Adams and Thomas Jefferson, who died 5 hours apart."
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("July 4, 2016")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
     context "Colloquial example 6" do
       text = "On October 3rd So-and-So asked me what day it was"
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("October 3, 2016")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
@@ -216,22 +216,22 @@ describe NaturalDateParsing do
       text = "Henry and Hanke created a calendar that causes each day to fall " +
       "on the same day of the week every year. They recommend its " +
       "implementation on January 1, 2018, a Monday."
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("January 1, 2018"),
                 Date.parse("July 11, 2016")] # Reconsider
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
     context "Colloquial example 8" do
       text = "Beyoncé Giselle Knowles-Carter was born on September 4, 1981."
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("September 4, 1981")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
@@ -239,11 +239,11 @@ describe NaturalDateParsing do
       text = "The first time I asked someone on a date, I was in Iowa, it was a " +
       "winterly mid-month January, we were to go to finding nemo, I was 9, and " +
       "although she was interested, her parents said no."
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("January 1st, 2016")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
@@ -251,11 +251,11 @@ describe NaturalDateParsing do
       text = "You ate 10 dates from a crate of dates that were picked on June " +
       "8th from a date tree that was cultivated one score and three years ago. " +
       "Good luck parsing through the antics of my semantics."
-      released = Date.parse("July 6, 2016")
+      creation_date = Date.parse("July 6, 2016")
       answer = [Date.parse("June 8, 2016")]
       
       it "correctly grabs the date" do
-        expect(NaturalDateParsing::interpret_date(text, released)).to eql(answer)
+        expect(NaturalDateParsing::interpret_date(text, creation_date)).to eql(answer)
       end
     end
     
